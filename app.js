1d3658baba6dcfed5f882943fb4fa6e406496479
Thomas Hampe <th.hampe@gmail.com>
2014-05-06 09:44:36 +0200

    Partial Router

--- app.js
+++ app.js
@@ -42,7 +42,20 @@
 
   app.get('/', function *(next){
     yield this.render('index', {});
-  })
+  });
+
+  app.get('/partials/*', function *(next) {
+      var stripped = this.req.url.split('.')[0];
+      var requestedView = path.join('./', stripped);
+      try {
+        yield this.render(requestedView, {layout: "partials/root"});
+      }catch(e) {
+        this.status = 404;
+        this.body = 404; 
+      }
+      
+
+  });
   
 
 }).call(this);
