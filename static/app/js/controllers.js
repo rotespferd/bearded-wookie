ea6eac0e7df902fa5a1681fae246982821e5599c
Marian Sievers <marian.sievers@gmail.com>
2014-05-13 10:02:32 +0200

    Get projects and ressources from server for overview-view

--- static/app/js/controllers.js
+++ static/app/js/controllers.js
@@ -1,17 +1,44 @@
 var ressourcesApp = angular.module('ressourcesApp', []);
+ressourcesApp.config(['$httpProvider',function ($httpProvider) {
+    $httpProvider.defaults.useXDomain = true;
+    delete $httpProvider.defaults.headers.common['X-Requested-With'];
+}]);
 
-ressourcesApp.controller('ProjectListCtrl', function ($scope) {
-  $scope.projects = [
-    {'name': 'Projekt 1'},
-    {'name': 'Projekt 2'},
-    {'name': 'Projekt 3'}
-  ];
+var baseURL = "http://ressourcenplannung.apiary-mock.com/";
+
+ressourcesApp.controller('OverviewCtrl', function ($scope, $http) {
+    $http.get(baseURL + 'projects').success(function(data) {
+        $scope.projects = data;
+
+        $scope.ressources = [];
+
+        _.each($scope.projects, function(element, index, list) {
+            var projectId = element.id;
+            console.log("Project id: " + projectId);
+            _.each(element.tasks, function(element, index, list){
+                console.log(element);
+                
+                if(!isObjectInArry(element, $scope.ressources)) {
+                    $http.get(baseURL + '/projects/' + projectId + '/tasks/' + element).success(function(data) {
+                        console.log(data);
+                        $scope.ressources.push(data);
+                    });
+                    
+                }
+            });
+        });
+    }).error(function (data) {
+        console.log("ERROR!");
+        $scope.projects = [];
+    });
+
+    /*$scope.ressources = [
+        {'name': 'Nexus S', 'project': 'Projekt 1'},
+        {'name': 'Motorola XOOM™ with Wi-Fi', 'project': 'Projekt 2'},
+        {'name': 'MOTOROLA XOOM™', 'project': 'Projekt 1'}
+    ];*/
 });
 
-ressourcesApp.controller('RessourcenListCtrl', function ($scope) {
-  $scope.ressources = [
-    {'name': 'Nexus S', 'project': 'Projekt 1'},
-    {'name': 'Motorola XOOM™ with Wi-Fi', 'project': 'Projekt 2'},
-    {'name': 'MOTOROLA XOOM™', 'project': 'Projekt 1'}
-  ];
-});
+function isObjectInArry(obj,arr) {
+    return (arr.indexOf(obj) != -1);
+}
